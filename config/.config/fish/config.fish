b6618826721392c0dba333b03b953caeab90ae62
Sampo Siltanen <sampo.siltanen@gmail.com>
2024-04-20 11:57:06 +0300

    Set kitty env paths required by homebrew installation
    These are auto generated by homebrew installation and I need to later
    check if they are actually needed or if there's a better way for this.

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -2,5 +2,12 @@ alias pn="pnpm"
 
 set -x DOTNET_NEW_PREFERRED_LANG F#
 
+set -gx HOMEBREW_PREFIX "/opt/homebrew";
+set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+! set -q PATH; and set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
+! set -q MANPATH; and set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
+! set -q INFOPATH; and set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+
 # Starship installation
 starship init fish | source
